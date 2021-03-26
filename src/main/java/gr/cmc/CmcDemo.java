7d35b5a238a044464159296d441ce368cf2a9c8a
John Daniel <jondan97@gmail.com>
2021-03-26 21:14:02 +0200

    Refactors ConverterService by adding documentation-splitting the code into methods-general improvements

--- src/main/java/gr/cmc/CmcDemo.java
+++ src/main/java/gr/cmc/CmcDemo.java
@@ -6,8 +6,8 @@ public class CmcDemo {
     public static void main(String[] args) {
         double number = 5;
 
-        BigDecimal result1 = ConverterService.convertBytes(number, DigitalMeasurement.MEGABYTE, DigitalMeasurement.KILOBYTE);
-        BigDecimal result2 = ConverterService.convertBytes(number, DigitalMeasurement.KILOBYTE, DigitalMeasurement.MEGABYTE);
+        BigDecimal result1 = ConverterService.convertBytes(number, DigitalMeasurement.KILOBYTE, DigitalMeasurement.MEGABYTE);
+        BigDecimal result2 = ConverterService.convertBytes(number, DigitalMeasurement.MEGABYTE, DigitalMeasurement.KILOBYTE);
         System.out.println(result1.doubleValue());
         System.out.println(result2.doubleValue());
     }
--- src/main/java/gr/cmc/ConverterService.java
+++ src/main/java/gr/cmc/ConverterService.java
@@ -4,23 +4,26 @@ import java.math.BigDecimal;
 
 public class ConverterService {
 
+    private static final BigDecimal conversionValueBD = new BigDecimal(1024);
+    private static final BigDecimal byteValueBD = new BigDecimal(8);
+
+
     public static BigDecimal convertBytes(double numberToBeConverted, DigitalMeasurement from, DigitalMeasurement to) {
-        BigDecimal convertedNumberBD = new BigDecimal(numberToBeConverted);
-        int conversionValue = 1024;
-        BigDecimal conversionValueBD = new BigDecimal(conversionValue);
-        int byteValue = 8;
-        BigDecimal byteValueBD = new BigDecimal(byteValue);
+        // Setting our initial variables
+        BigDecimal bigDecimalToBeConverted = new BigDecimal(numberToBeConverted);
+        // If the conversion ends or begins from/to bit
         boolean isBitsConversion = false;
 
-        int startingPosition = from.getPosition();
-        int endingPosition = to.getPosition();
-        int distance = startingPosition - endingPosition;
+        int distance = calculateDistance(from, to);
 
-        if (startingPosition == 1 || endingPosition == 1) {
+        if (from.getPosition() == 1 || to.getPosition() == 1) {
             isBitsConversion = true;
         }
 
+        return convert(bigDecimalToBeConverted, isBitsConversion, distance);
+    }
 
+    private static BigDecimal convert(BigDecimal convertedNumberBD, boolean isBitsConversion, int distance) {
         if (distance > 0) {
             if (isBitsConversion) {
                 convertedNumberBD = convertedNumberBD.multiply(byteValueBD);
@@ -38,7 +41,12 @@ public class ConverterService {
                 convertedNumberBD = convertedNumberBD.divide(conversionValueBD);
             }
         }
-
         return convertedNumberBD;
+    }
+
+    private static int calculateDistance(DigitalMeasurement from, DigitalMeasurement to) {
+        int startingPosition = from.getPosition();
+        int endingPosition = to.getPosition();
+        return startingPosition - endingPosition;
     }
 }
